749ef4e78288d8849b79da79e8959a266eb7db34
Dmytro-Avramenko <himera33a@gmail.com>
2023-05-07 15:04:33 +0300

    config change

---
+++ config.js
@@ -0,0 +1,5 @@
+const DB_HOST = 'mongodb+srv://Dmytro:pXew3zNxP6XEnPtX@cluster0.shnkl8a.mongodb.net/db-contacts?retryWrites=true&w=majority' 
+
+module.exports = {
+  DB_HOST,
+}
--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const app = require('./app')
 
-const DB_HOST = 'mongodb+srv://Dmytro:pXew3zNxP6XEnPtX@cluster0.shnkl8a.mongodb.net/db-contacts?retryWrites=true&w=majority' 
+// const { DB_HOST } = require('./config')
+const { DB_HOST } = process.env;
 
 mongoose.set('strictQuery', true);
 
